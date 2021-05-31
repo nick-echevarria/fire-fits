2f582184a5f0114a598542f4e50d4d7673de77a2
nick-echevarria <nick.echev@gmail.com>
2021-05-31 15:16:07 -0400

    form upload w/ hook

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d9e3d5497d3509a088c4";
+/******/ 	var hotCurrentHash = "1bda9919abe635a167f7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/createProduct.js
+++ components/createProduct.js
@@ -1,44 +1,76 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
+
 
 export default function createProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'Nice shoes',
     price: 34245,
     description: 'These are the best!',
   });
 
   return (
-    <form>
-      <label htmlFor="name">
-        Name
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input
-          type="number"
-          id="price"
-          name="price"
-          placeholder="Price"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      {/* use existing aria attribute for loading state */}
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
 
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
+        <button type="button" onClick={clearForm}>
+          Clear Form
+        </button>
 
-      <button type="button" onClick={resetForm}>
-        Reset Form
-      </button>
-    </form>
+        <button type="button" onClick={resetForm}>
+          Reset Form
+        </button>
+
+        <button type="ubmit">Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
