f736c92568a964add05cc76f6d9f236150b2ef4f
nick-echevarria <nick.echev@gmail.com>
2021-05-31 17:24:14 -0400

    Added refetchQuery for pessimistic rendering

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bda9919abe635a167f7";
+/******/ 	var hotCurrentHash = "02dd50670b85b9ba366a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/createProduct.js
+++ components/createProduct.js
@@ -1,5 +1,9 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -38,6 +42,8 @@ export default function createProduct() {
     {
       // any additional data that comes along
       variables: inputs,
+      // we'll need the below to refresh UI using updated server data
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
 
